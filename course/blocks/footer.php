726b43652c37c2b807d85ee3e2e72845b80de65e
_Allex_ <91027382+AlexeySilchenko02@users.noreply.github.com>
2022-01-12 22:23:00 +0300

    Add files via upload

--- course/blocks/footer.php
+++ course/blocks/footer.php
@@ -1,8 +1,10 @@
 <footer>
+    <div class = "text-center fixed-bottom">
     Все права защищены &copy; 2022 <br>
     <?php
     echo date('d.m.Y H:i:s') . '<br>';
     ?>
+    </div>
 </footer>
 </body>
 </html>
--- course/index.php
+++ course/index.php
@@ -2,7 +2,20 @@
     $title = "Главная";
     require "blocks/header.php";
     ?>
-<h1>Главная страница</h1>
+        
+    <div class = "authorization">
+        <p>Авторизация</p>
+    </div>
+    
+    <div class = "text-center container-fluid">
+    <h1>Поиск спортивных залов в вашем районе</h1>
+    <form action="gyms.php" method = "POST">
+    <input type="text" name = "username" placeholder = "Введите ваш район" class = "form-control w-50 p-3 container-fluid text-center mt-3"><br>
+    <input type="submit" value = "Отправить" class = "btn btn-success">
+    </form>
+    </div>
+
+
 <?php
     require "blocks/footer.php";
     ?>
